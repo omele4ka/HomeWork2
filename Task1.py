09c47c938ae731f36feb9605524cd55777127326
Omele4ka <omele4ka@inbox.ru>
2022-11-27 14:19:45 +0300

    Task 1 is solved

--- Task1.py
+++ Task1.py
@@ -2,4 +2,15 @@
 # Пример:
 # 6782 -> 23
 # 0,56 -> 11
-#
+
+num = input('Введите число: ')
+def sum_digit(num):
+    num = num.split('.')
+    sum = 0
+    for i in range(len(num)):
+        digit = int(num[i])
+        while digit != 0:
+            sum += digit % 10
+            digit = digit // 10
+    return sum
+print(f'Сумма цифр в числе {num} равна {sum_digit(num)}')
