da799cebefbca4cdfac2915d704fb402437f4018
Omele4ka <omele4ka@inbox.ru>
2022-11-27 15:12:32 +0300

    Task 3 is solved

--- Task3.py
+++ Task3.py
@@ -1,2 +1,21 @@
 # Реализуйте алгоритм перемешивания списка.
 # Встроенный алгоритм SHUFFLE не использовать! Реализовать свой метод
+
+import random
+my_list = []
+for i in range(5):
+    my_list.append(i)
+print(f'Исходный список {my_list}')
+
+def shuffle_elem(original_list):
+    new_list = original_list[:]
+    new_list_length = len(new_list)
+    for i in range(new_list_length):
+        new_list_index = random.randint(0, new_list_length - 1)
+        temp = new_list[i]
+        new_list[i] = new_list[new_list_index]
+        new_list[new_list_index] = temp
+    return new_list
+print(f'Новый список {shuffle_elem(my_list)}')
+
+
