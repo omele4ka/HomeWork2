d5e7a564c654fe376fe3036e660f29c21f7f6652
Omele4ka <omele4ka@inbox.ru>
2022-11-27 14:33:32 +0300

    Task 2 is solved

--- Task2.py
+++ Task2.py
@@ -1,2 +1,12 @@
 # Задайте список из n чисел последовательности (1 + 1/n)^n.
-# Вывестив консоль сам список и сумму его элементов.
+# Вывести в консоль сам список и сумму его элементов.
+
+num = int(input('Введите количество чисел: '))
+my_list = []
+for i in range(1, num + 1):
+    my_list.append((1 + 1/i) ** i)
+print(my_list)
+sum_elem = 0
+for j in range(len(my_list)):
+    sum_elem += my_list[j]
+print(f'Сумма элементов списка {sum_elem}')
